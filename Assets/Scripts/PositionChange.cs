7c190036b7f0e0b369800003eb809af9a0d3815a
JoseLucioS <dragaolucio@gmail.com>
2022-07-11 10:48:59 -0300

    Add: object's movement left-right script

---
+++ Assets/Scripts/PositionChange.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionChange : MonoBehaviour
+{
+    //public Vector3 positionChange;
+    private int direction = 1;
+    private float maxValue = 5f;
+    private float minValue = -5f;
+    private float currentValue = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentValue += direction * Time.deltaTime;
+
+        if (currentValue >= maxValue)
+        {
+            direction *= -1;
+            currentValue = maxValue;
+        }
+        else if(currentValue <= minValue)
+        {
+            direction *= -1;
+            currentValue = minValue;
+        }
+
+        //move the object on the axis X
+        transform.position = new Vector3(currentValue, transform.position.y, transform.position.z);
+    }
+}
